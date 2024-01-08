cf51e0cebf98c5c2e18d237534725f1a2d1058db
yaongdaong <haeryang2@naver.com>
2024-01-08 21:53:43 +0900

    check

---
+++ src/main/java/com/example/springdeveloper/SpringdeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdeveloper/SpringdeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
